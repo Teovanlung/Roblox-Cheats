9b1aaa4c7c3cbde00d9fc5afc318c44ae7d225b0
Teovanlung <160012482+Teovanlung@users.noreply.github.com>
2024-08-02 06:05:08 +0700

    Delete Super Sword.lua

--- Super Sword.lua
+++
@@ -1,7 +0,0 @@
-local SIGNAL_REMOTE = game:GetService("ReplicatedStorage"):WaitForChild("SIGNAL_REMOTE")
-while true do
-  SIGNAL_REMOTE:FireServer("ActivateHitbox", "Lunge")
-  task.wait(0.3)
-  SIGNAL_REMOTE:FireServer("ActivateHitbox", "Strike")
-  task.wait(0.4)
-end
